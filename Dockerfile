1296c65015d2f999f0df7d97bd517d3ed6009522
SIGNING <arollito1@gmail.com>
2023-07-11 22:36:46 +0100

    second

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+#FROM eclipse-temurin:17-jdk-jammy
+#WORKDIR /app
+#
+#COPY .mvn .mvn
+#COPY mvnw pom.xml ./
+#RUN ./mvnw dependency:go-offline
+#
+#COPY src ./src
+#CMD ["./mvnw","spring-boot:run"]
+
+
+FROM openjdk
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/example/post/PostApplication.java
+++ src/main/java/com/example/post/PostApplication.java
@@ -1,23 +1,67 @@
 package com.example.post;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-
 public class PostApplication {
+    private final UserRepo userRepo;
+
+    public PostApplication(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PostApplication.class, args);
     }
 
     @GetMapping
-    public ResponseEntity<String> getHello(){
-        return ResponseEntity.ok("hello, by Arollito!.");
+    public ResponseEntity<User> getHello() {
+        return ResponseEntity.ok(userRepo.findById(1L).get());
     }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            userRepo.save(new User(
+                    null, "arol", "user", 'M', "arollito1@gamail.com"
+            ));
+        };
+    }
+}
+
+@Entity
+@Table(name = "user", schema = "prod_db")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String role;
+    private char gender;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
+
+@Repository
+interface UserRepo extends JpaRepository<User, Long> {
+
 }
